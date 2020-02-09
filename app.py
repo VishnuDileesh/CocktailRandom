607d946621d75b5dd59fdd5bd9e04b8ed8fe3786
Pheyonix <dev@vishnudileesh.com>
2020-02-09 18:49:16 +0530

    Call api and get cocktail name and thumbnail image

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ import requests
 
 app = Flask(__name__)
 
+def getRandomCocktail():
+
+    r = requests.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+
+    cocktail = r.json()
+
+    #print(cocktail['drinks'][0]['strDrink'])
+
+    cocktail_name = cocktail['drinks'][0]['strDrink']
+
+    cocktail_thumb = cocktail['drinks'][0]['strDrinkThumb']
+
+    print(cocktail_thumb)
+
 @app.route('/')
 def index(): 
+
+    getRandomCocktail()
 
     return "Hello World"
 
